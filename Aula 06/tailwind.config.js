a3d6b0ebdc41d32233e53acbcceb10537411d46f
Luan Mateus <luanmateus2002@gmail.com>
2024-04-07 11:23:36 -0400

    Aula 6
    pagina usando tailwindcss

---
+++ Aula 06/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      fontFamily: {
+        'fonte': ['Open Sans', 'sans-serif',],     
+      },
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  
